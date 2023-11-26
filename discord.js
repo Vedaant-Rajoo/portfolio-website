36b42c395a8de1fd0ca28c5356d0b0056bde99a3
Vedaant-Rajoo <vedaant12345@gmail.com>
2023-11-26 15:50:59 +0530

    Delete unused files and update Discord and Spotify status display

--- discord.js
+++ discord.js
@@ -1,63 +1,65 @@
-// const webSocket = new WebSocket("wss://api.lanyard.rest/socket");
-// const discordID = "381767483100626945";
+const webSocket = new WebSocket('wss://api.lanyard.rest/socket');
+const discordID = '381767483100626945';
 
-// function updateStatus(data) {
-//   const statusElement = document.getElementById("discord-status-highlight");
-//   const spotifySongElement = document.getElementById("spotify-song");
-//   const spotifyArtistElement = document.getElementById("spotify-artist");
+function updateStatus(data) {
+  const statusElement = document.getElementById('discord-status-highlight');
+  const spotifySongElement = document.getElementById('spotify-song');
+  const latestAnime = document.getElementById('latestAnime');
+  const githubRepo = document.getElementById('github-repo');
 
-//   // Update Discord status
-//   const statusColorMap = {
-//     online: "#23a55a",
-//     idle: "#f0b232",
-//     dnd: "#f23f43",
-//     offline: "#80848e"
-//   };
-//   const statusText = data.discord_status ? data.discord_status.replace('dnd', 'Do not disturb') : 'Offline';
-//   statusElement.innerText = statusText.charAt(0).toUpperCase() + statusText.slice(1);
-//   statusElement.style.color = statusColorMap[data.discord_status] || "#80848e";
+  // Update Discord status
+  const statusColorMap = {
+    online: '#23a55a',
+    idle: '#f0b232',
+    dnd: '#f23f43',
+    offline: '#80848e',
+  };
+  const statusText = data.discord_status
+    ? data.discord_status.replace('dnd', 'Do not disturb')
+    : 'Offline';
+  statusElement.innerText =
+    statusText.charAt(0).toUpperCase() + statusText.slice(1);
+  statusElement.style.color = statusColorMap[data.discord_status] || '#80848e';
 
-//   // Update Spotify status
-//   if (data.listening_to_spotify) {
-//     spotifySongElement.innerText = `listening to ${data.spotify.song}`;
-//     spotifyArtistElement.innerText = ` by ${data.spotify.artist.replaceAll(";", ",")}`;
-//   } else {
-//     spotifySongElement.innerText = "";
-//     spotifyArtistElement.innerText = "";
-//   }
-// }
+  // Update Spotify status
+  if (data.listening_to_spotify) {
+    spotifySongElement.innerText = `${
+      data.spotify.song
+    } by ${data.spotify.artist.replaceAll(';', ',')}`;
+  } else {
+    spotifySongElement.innerText = 'Nothing!';
+  }
+  latestAnime.innerText = data.kv.latestAnime;
+  const repoName = data.kv.githubrepo || 'Nothing!';
+  const repoLink = data.kv.githubrepolink || '#';
+  githubRepo.innerHTML = `<a href="${repoLink}" target="_blank">${repoName}</a>`;
+}
 
-// fetch(`https://api.lanyard.rest/v1/users/${discordID}`)
-//   .then(response => response.json())
-//   .then(e => { 
-//     if (e.data["discord_user"]) {
-//       updateStatus(e.data);
-//     }
-//   });
+fetch(`https://api.lanyard.rest/v1/users/${discordID}`)
+  .then((response) => response.json())
+  .then((e) => {
+    if (e.data['discord_user']) {
+      updateStatus(e.data);
+    }
+  });
 
-// webSocket.addEventListener("message", event => {
-//   const data = JSON.parse(event.data);
+webSocket.addEventListener('message', (event) => {
+  const data = JSON.parse(event.data);
 
-//   if (event.data === '{"op":1,"d":{"heartbeat_interval":30000}}') {
-//     webSocket.send(JSON.stringify({ op: 2, d: { subscribe_to_id: discordID }}));
-//     setInterval(() => webSocket.send(JSON.stringify({ op: 3, d: { heartbeat_interval: 30000 }})), 30000);
-//   }
+  if (event.data === '{"op":1,"d":{"heartbeat_interval":30000}}') {
+    webSocket.send(
+      JSON.stringify({ op: 2, d: { subscribe_to_id: discordID } }),
+    );
+    setInterval(
+      () =>
+        webSocket.send(
+          JSON.stringify({ op: 3, d: { heartbeat_interval: 30000 } }),
+        ),
+      30000,
+    );
+  }
 
-//   if (data.t === "PRESENCE_UPDATE") {
-//     updateStatus(data.d);
-//   }
-// });
-// // Select the element that triggers the hover effect
-// const navbarName = document.querySelector('#navbarName'); // Replace with your actual element ID or class
-
-// // Select the element to show/hide
-// const statusElement = document.querySelector('#discord-status');
-
-// // Add hover event listeners
-// navbarName.addEventListener('mouseover', () => {
-//   gsap.to(statusElement, { duration: 0.5, autoAlpha: 1 }); // Fade in
-// });
-
-// navbarName.addEventListener('mouseout', () => {
-//   gsap.to(statusElement, { duration: 0.5, autoAlpha: 0 }); // Fade out
-// });
+  if (data.t === 'PRESENCE_UPDATE') {
+    updateStatus(data.d);
+  }
+});
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Select your name logo element
-const nameLogo = document.querySelector("#navbarName"); // Replace with the actual ID or class
+const nameLogo = document.querySelector('#navbarName'); // Replace with the actual ID or class
 
 // Hover animation
 nameLogo.addEventListener('mouseover', () => {
   gsap.to(nameLogo, {
     duration: 0.5, // Duration of the animation
     scale: 1.1, // Slightly increase the scale
-    color: "#008080", // Change to a highlight color
-    ease: "power1.inOut" // Smoothing the animation
+    color: '#008080', // Change to a highlight color
+    ease: 'power1.inOut', // Smoothing the animation
   });
 });
 
@@ -16,37 +16,68 @@ nameLogo.addEventListener('mouseout', () => {
   gsap.to(nameLogo, {
     duration: 0.5,
     scale: 1, // Reset scale
-    color: "#1C1917", // Reset color
-    ease: "power1.inOut"
+    color: '#1C1917', // Reset color
+    ease: 'power1.inOut',
   });
 });
-
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('nav');
-    if (window.scrollY > 0) {
-        navbar.classList.add('border-stone-200');
-        navbar.classList.remove('border-transparent');
-    } else {
-        navbar.classList.add('border-transparent');
-        navbar.classList.remove('border-stone-200');
+function updateNavIndicator() {
+  const sections = ['hi', 'about', 'work', 'contact']; // Ensure these are the IDs of your sections
+  let activeSection = '';
+  
+  sections.forEach((sectionId, index) => {
+    const section = document.getElementById(sectionId);
+    const nextSection = sections[index + 1] ? document.getElementById(sections[index + 1]) : null;
+    const rect = section.getBoundingClientRect();
+    const nextRect = nextSection ? nextSection.getBoundingClientRect() : null;
+    
+    if ((nextRect && rect.top <= 0 && nextRect.top > 0) || (!nextRect && rect.top <= window.innerHeight / 2)) {
+      activeSection = sectionId;
     }
+  });
+
+  document.querySelectorAll('nav ul li span').forEach(span => {
+    span.classList.remove('opacity-100');
+  });
+
+  if (activeSection) {
+    const activeLink = document.querySelector(`nav a[href="#${activeSection}"]`);
+    if (activeLink) {
+      activeLink.parentNode.querySelector('span').classList.add('opacity-100');
+    }
+  }
+  console.log(activeSection);
+}
+
+window.addEventListener('scroll', updateNavIndicator);
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', () => setTimeout(updateNavIndicator, 300)); // Adjust the timeout as needed
+});
+
+updateNavIndicator(); // Initial call to set the indicator on page load
+
+
+window.addEventListener('scroll', function () {
+  var navbar = document.getElementById('nav');
+  if (window.scrollY > 0) {
+    navbar.classList.add('border-stone-200');
+    navbar.classList.remove('border-transparent');
+  } else {
+    navbar.classList.add('border-transparent');
+    navbar.classList.remove('border-stone-200');
+  }
 });
 
 document
   .querySelector('.cursor-pointer')
   .addEventListener('mouseover', function (e) {
-    if(window.innerWidth >= 1024) {
+    if (window.innerWidth >= 1024) {
       document.querySelector('.cursor-pointer').style.cursor = 'none';
       const cursor = document.getElementById('customCursor');
       cursor.classList.remove('scale-50', 'opacity-0');
-      cursor.classList.add(
-        'scale-100',
-        'opacity-100',
-      );
+      cursor.classList.add('scale-100', 'opacity-100');
       cursor.style.top = `${e.pageY}px`;
       cursor.style.left = `${e.pageX}px`;
-    }
-    else{
+    } else {
       document.querySelector('.cursor-pointer').style.cursor = 'pointer';
     }
   });
@@ -58,12 +89,8 @@ document
       document.querySelector('.cursor-pointer').style.cursor = 'none';
       const cursor = document.getElementById('customCursor');
       cursor.classList.add('scale-50', 'opacity-0');
-      cursor.classList.remove(
-        'scale-100',
-        'opacity-100',
-      );
-    }
-    else{
+      cursor.classList.remove('scale-100', 'opacity-100');
+    } else {
       document.querySelector('.cursor-pointer').style.cursor = 'pointer';
     }
   });
@@ -73,3 +100,22 @@ document.addEventListener('mousemove', function (e) {
   cursor.style.top = `${e.pageY}px`;
   cursor.style.left = `${e.pageX}px`;
 });
+
+document
+  .querySelector('.cursor-pointer')
+  .addEventListener('click', function () {
+    const cursor = document.getElementById('customCursor');
+    const copyText = document.getElementById('copyText');
+
+    // Change text and update color to text-stone-900
+    cursor.innerHTML = 'Done!';
+    cursor.classList.remove('text-green-700');
+    copyText.innerHTML = 'Copied!';
+
+    // Set a timeout to revert the text and color back to the original state (text-green-900) after 5 seconds
+    setTimeout(function () {
+      cursor.innerHTML = 'Copy'; // Assuming 'Copy' is the original text
+      cursor.classList.add('text-green-700');
+      copyText.innerHTML = 'Click email to copy';
+    }, 5000);
+  });
