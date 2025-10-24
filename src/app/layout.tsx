67d90279337da66c655334e575306c226e663fcc
vedaant-rajoo <vedaant12345@gmail.com>
2025-10-24 04:42:11 -0700

    refactor(layout): remove animated cursor components and associated imports for cleaner layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono, Crimson_Pro } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
-import { Cursor, CursorProvider } from '@/components/ui/shadcn-io/animated-cursor';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -37,23 +36,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${crimsonPro.variable} antialiased`}
       >
-        <CursorProvider>
-          <Cursor>
-            <svg
-              className='size-6 text-blue-500'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 40 40'
-            >
-              <path
-                fill='currentColor'
-                d='M1.8 4.4 7 36.2c.3 1.8 2.6 2.3 3.6.8l3.9-5.7c1.7-2.5 4.5-4.1 7.5-4.3l6.9-.5c1.8-.1 2.5-2.4 1.1-3.5L5 2.5c-1.4-1.1-3.5 0-3.3 1.9Z'
-              />
-            </svg>
-          </Cursor>
-          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-            {children}
-          </ThemeProvider>
-        </CursorProvider>
+        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
